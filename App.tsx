7a576a02ff06535bbdf5d6f72592bd0c13d40b6b
Giorbis Miguel <giorbismiguel@gmail.com>
2021-09-18 01:38:50 -0400

    Add package status bar an safe area context

--- App.tsx
+++ App.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { StatusBar } from 'expo-status-bar'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <SafeAreaView>
+      <StatusBar />
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
